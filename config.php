eda395e25d9a1faa8982c0eff79bb3abfefa039c
handimanny <handimanny10@gmail.com>
2022-09-29 14:07:59 +0700

    perbarui

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 $db_servername = "localhost";
 $db_username = "root";
 $db_password = "";
-$db_name = "db";
+$db_name = "perpustakaan";
 
-$db = mysqli_connect($db_servername, $db_username, $db_password, $db_name);
+$perpustakaan = mysqli_connect($db_servername, $db_username, $db_password, $db_name);
 
-if (!$db) {
+if (!$perpustakaan) {
     die("<script>alert('Gagal tersambung dengan database.')</script>");
 }
 
