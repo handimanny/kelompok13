6c3f120e6dd01b102d60bcc0019cd69f2ab55e95
agungdwih <agungdwi.hartanto3@gmail.com>
2022-10-02 14:40:03 +0700

    buku13

--- main/admin/petugas.php
+++ main/admin/petugas.php
@@ -94,9 +94,9 @@ if(isset($_GET['cari'])){
     <p class="menu-label">Umum</p>
     <ul class="menu-list">
       <li class="active">
-        <a href="admin.php">
+        <a href="dashboard.php">
           <span class="icon"><i class="mdi mdi-desktop-mac"></i></span>
-          <span class="menu-item-label">Halaman</span>
+          <span class="menu-item-label">Dashboard</span>
         </a>
       </li>
     </ul>
@@ -115,7 +115,7 @@ if(isset($_GET['cari'])){
         </a>
       </li>
       <li class="--set-active-profile-html">
-        <a href="buat.php">
+        <a href="buku.php">
           <span class="icon"><i class="mdi mdi-book"></i></span>
           <span class="menu-item-label">Buku</span>
         </a>
@@ -162,11 +162,48 @@ if(isset($_GET['cari'])){
 
 <!-- main section -->
 <table class="table table-primary p-1 mt-4 border border-primary container">
+  <tbody>
+      <tr>
+          <th>NIP</th>
+          <th>Nama</th>
+          <th>Jenis Kelamin</th>
+          <th>Alamat</th>
+          <th class="text-center" >Update</th>
+      </tr>
+  </tbody>
 
-<center>
-  Tampilan taruh sini
-</center>  
+  <tbody>
+  <?php
+    
+    if(isset($_GET['cari'])){
+      $cari = $_GET['cari'];
+      $result = mysqli_query($conn,"SELECT * FROM petugas WHERE nama LIKE '%".$cari."%'");				
+    }else{
+      $result = mysqli_query($conn,"SELECT * FROM petugas");
+    }
+    $no =1;
+    while($data = mysqli_fetch_array($result)) {         
+      ?>
+        <tbody>
+        <tr>
+            <td class="text-center"><?= $no ?></td>
+            <td><?= $data['1']?></td>
+            <td><?= $data['2']?></td>
+            <td><?= $data['3']?></td>
 
+            <td colspan="2">            
+            
+            <a href="edit.php?id_buku=<?=$data['nip']?>" class="btn btn-outline-primary">Edit</a>
+            |
+            <a href="delete.php?id_buku=<?=$data['nip']?>" class="btn btn-outline-danger">Hapus</a>
+            </td>
+
+        </tr>
+        </tbody>
+    <?php
+    $no++;
+      }
+      ?>
 </table>
 <!-- end main section -->
         <div class="table-pagination">
