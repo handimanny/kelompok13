0cbe32fd7de19597c62db2eca810bc66d5c4631d
agungdwih <agungdwi.hartanto3@gmail.com>
2022-10-02 21:52:57 +0700

    baru13

--- main/admin/buatsiswa.php
+++ main/admin/buatsiswa.php
@@ -21,7 +21,7 @@ $result= mysqli_query($conn,$sql);
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Tambah Buku</title>
+  <title>Tambah Siswa</title>
 
   <!-- Tailwind is included -->
   <link rel="stylesheet" href="css/main.css?v=1628755089081">
