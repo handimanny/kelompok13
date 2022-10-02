a469e297d4a0584097b2c790de82828989842917
agungdwih <agungdwi.hartanto3@gmail.com>
2022-10-02 21:26:52 +0700

    baru13

--- main/admin/buatkelas.php
+++ main/admin/buatkelas.php
@@ -161,7 +161,7 @@ $result= mysqli_query($conn,$sql);
 <section class="is-hero-bar">
   <div class="flex flex-col md:flex-row items-center justify-between space-y-6 md:space-y-0">
     <h1 class="title">
-      Forms Tambah Buku
+      Forms Tambah Siswa
     </h1>
   </div>
 </section>
